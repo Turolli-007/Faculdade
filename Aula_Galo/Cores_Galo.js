a00dcfc417e3fb6d9cf3faa1a6e5dd79da7a8b01
Turolli-007 <oturolli32@gmail.com>
2026-05-11 20:21:32 -0300

    Create Cores_Galo.js

---
+++ Aula_Galo/Cores_Galo.js
@@ -0,0 +1,7 @@
+let cores = ["vermelho", "verde", "azul"];
+
+console.log(cores.includes("verde"));
+console.log(cores.includes("roxo"));
+
+console.log(cores.indexOf("roxo"));
+console.log(cores.indexOf("azul"));
